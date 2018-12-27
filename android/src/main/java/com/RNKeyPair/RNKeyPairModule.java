92137d92eb37b8452c787749d298f70cb461d98d
sarikabhimre18 <sarikabhimre18@gmail.com>
2018-12-28 00:04:16 +0530

    Use RSA algorithm and Generate PKCS1 private key

--- android/src/main/java/com/RNKeyPair/RNKeyPairModule.java
+++ android/src/main/java/com/RNKeyPair/RNKeyPairModule.java
@@ -8,6 +8,14 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.WritableNativeMap;
 
+import org.spongycastle.asn1.ASN1Encodable;
+import org.spongycastle.asn1.ASN1Primitive;
+import org.spongycastle.asn1.pkcs.PrivateKeyInfo;
+import org.spongycastle.util.io.pem.PemObject;
+import org.spongycastle.util.io.pem.PemWriter;
+
+import java.io.IOException;
+import java.io.StringWriter;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
 import java.security.KeyPair;
@@ -32,20 +40,45 @@ public class RNKeyPairModule extends ReactContextBaseJavaModule {
         return "RNKeyPair";
     }
 
-    public static String getPrivateKeyPKCS8String(PrivateKey priv) throws GeneralSecurityException {
-        KeyFactory fact = KeyFactory.getInstance("DSA");
-        PKCS8EncodedKeySpec spec = fact.getKeySpec(priv,
-                PKCS8EncodedKeySpec.class);
-        byte[] packed = spec.getEncoded();
-        String key64 = new String(Base64.encode(packed, 0));
+    public static String getPrivateKeyPKCS8String(byte[] priv) throws GeneralSecurityException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        KeyPair keyPair = kpg.genKeyPair();
+        byte[] publicKey = keyPair.getPublic().getEncoded();
+        byte[] privateKey = keyPair.getPrivate().getEncoded();
 
-        Arrays.fill(packed, (byte) 0);
-        return "-----BEGIN PRIVATE KEY-----\n" + key64 + "-----END PRIVATE KEY-----";
+        PrivateKeyInfo pkInfo = PrivateKeyInfo.getInstance(priv);
+        ASN1Encodable encodeable = null;
+        try {
+            encodeable = pkInfo.parsePrivateKey();
+        } catch (IOException e) {
+
+
+        }
+        ASN1Primitive primitive2 = encodeable.toASN1Primitive();
+        byte[] privateKeyPKCS1 = new byte[0];
+        try {
+            privateKeyPKCS1 = primitive2.getEncoded();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        PemObject pemObject2 = new PemObject("RSA PRIVATE KEY", privateKeyPKCS1);
+        StringWriter stringWriter2 = new StringWriter();
+        PemWriter pemWriter2 = new PemWriter(stringWriter2);
+        try {
+            pemWriter2.writeObject(pemObject2);
+            pemWriter2.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stringWriter2.toString();
     }
 
 
     public static String getPublicKeyX509String(PublicKey publ) throws GeneralSecurityException {
-        KeyFactory fact = KeyFactory.getInstance("DSA");
+        KeyFactory fact = KeyFactory.getInstance("RSA");
         X509EncodedKeySpec spec = fact.getKeySpec(publ,
                 X509EncodedKeySpec.class);
         return "-----BEGIN PUBLIC KEY-----\n" +
@@ -70,9 +103,10 @@ public class RNKeyPairModule extends ReactContextBaseJavaModule {
             PrivateKey privateKeyPKCS8 = kf.generatePrivate(new PKCS8EncodedKeySpec(privateKey));
 
             keys.putString("public", getPublicKeyX509String(publicKeyX509));
-            keys.putString("private", getPrivateKeyPKCS8String(privateKeyPKCS8));
+            keys.putString("private", getPrivateKeyPKCS8String(privateKey));
         }
         catch(GeneralSecurityException e) { }
         callback.invoke(keys);
     }
 }
+
